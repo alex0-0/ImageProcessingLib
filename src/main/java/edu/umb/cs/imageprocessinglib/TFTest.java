0cca0340d62aae9dba5e4fc1e6dc37e9386e2c2f
alex <alexande2008@gmail.com>
2019-03-31 18:15:06 -0400

    Deep copy cropped image

--- src/main/java/edu/umb/cs/imageprocessinglib/TFTest.java
+++ src/main/java/edu/umb/cs/imageprocessinglib/TFTest.java
@@ -5,6 +5,7 @@ import edu.umb.cs.imageprocessinglib.model.Recognition;
 import edu.umb.cs.imageprocessinglib.util.ImageUtil;
 import org.opencv.core.Core;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class TFTest {
     public static void main(String[] args) throws Exception {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         rotationTest();
         sizeTest();
     }
@@ -39,13 +40,13 @@ public class TFTest {
 
         List<BufferedImage> images = divideImage(img, 3, 3);
         for (BufferedImage i : images) {
-            List<Recognition> rs = objectDetector.recognizeImage(img);
+            List<Recognition> rs = objectDetector.recognizeImage(i);
 
             for (Recognition r : rs) {
                 System.out.printf("Object: %s - confidence: %f box: %s\n",
                         r.getTitle(), r.getConfidence(), r.getLocation());
-                BoxPosition bp = r.getScaledLocation((float)img.getWidth()/r.getModelSize(), (float)img.getHeight()/r.getModelSize());
-                BufferedImage t = img.getSubimage(bp.getLeftInt(), bp.getTopInt(), bp.getWidthInt(), bp.getHeightInt());
+                BoxPosition bp = r.getScaledLocation((float)i.getWidth()/r.getModelSize(), (float)i.getHeight()/r.getModelSize());
+                BufferedImage t = i.getSubimage(bp.getLeftInt(), bp.getTopInt(), bp.getWidthInt(), bp.getHeightInt());
                 ImageUtil.displayImage(t);
             }
 
@@ -77,7 +78,23 @@ public class TFTest {
             }
         }
 
-        return imgs;
+        List<BufferedImage> r = new ArrayList<>();
+        //deep copy
+        for (BufferedImage i : imgs) {
+            BufferedImage deepCopy = new BufferedImage(i.getWidth(), i.getHeight(), i.getType());
+
+        // Draw the subimage onto the new, empty copy
+            Graphics2D g = deepCopy.createGraphics();
+            try {
+                g.drawImage(i, 0, 0, null);
+            }
+            finally {
+                g.dispose();
+            }
+            r.add(deepCopy);
+        }
+
+        return r;
     }
 
 }
